f70272f128e0b9993a225baa73010bfe0adbdf75
Yellowstonemc12 <yellowstonemc12@gmail.com>
2026-06-03 22:22:22 +0800

    Update index.py
    Signed-off-by: Yellowstonemc12 <yellowstonemc12@gmail.com>

--- api/index.py
+++ api/index.py
@@ -1,22 +1,31 @@
+# from fastapi import FastAPI
+# from supabase import create_client
+# import os
+
+# app = FastAPI()
+
+# supabase = create_client(
+#     os.environ["SUPABASE_URL"],
+#     os.environ["SUPABASE_KEY"]
+# )
+
+# @app.get("/")
+# def home():
+#     result = supabase.table("homework").insert({
+#         "username": "test",
+#         "homework": "Math"
+#     }).execute()
+
+#     return {
+#         "success": True,
+#         "data": result.data
+#     }
+
+
 from fastapi import FastAPI
-from supabase import create_client
-import os
 
 app = FastAPI()
 
-supabase = create_client(
-    os.environ["SUPABASE_URL"],
-    os.environ["SUPABASE_KEY"]
-)
-
 @app.get("/")
 def home():
-    result = supabase.table("homework").insert({
-        "username": "test",
-        "homework": "Math"
-    }).execute()
-
-    return {
-        "success": True,
-        "data": result.data
-    }
+    return {"hello": "world"}
